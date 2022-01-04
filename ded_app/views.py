52a5191e7b7050672cad3c4b9b36f2fa23269240
Skibicki-S <stas.skibickiy32@gmail.com>
2022-01-04 12:06:31 +0300

    start new project.

---
+++ ded_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+menu = [{'boss': 'Главная', 'url_name': 'index'},
+        {'boss': 'Письмо Дедушке Морозу', 'url_name': 'letter'},
+        {'boss': 'Интересные факты', 'url_name': 'fact'},
+        {'boss': 'Галерея', 'url_name': 'gallery'},
+        {'boss': 'Заказать на дом', 'url_name': 'order'},
+        {'boss': 'Контакты', 'url_name': 'contact'},
+        ]
