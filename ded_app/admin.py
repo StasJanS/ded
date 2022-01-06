a675938bb0d8fa187a796938a40c82ff07e6d90b
Skibicki-S <stas.skibickiy32@gmail.com>
2022-01-06 15:36:05 +0300

    add 2 model.

--- ded_app/admin.py
+++ ded_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Foto, Jear
+
+admin.site.register(Foto)
+admin.site.register(Jear)
--- ded_app/models.py
+++ ded_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Jear(models.Model):
+    number = models.CharField(max_length=10, verbose_name='Год')
+
+
+class Foto(models.Model):
+    img = models.ImageField(verbose_name='Фото')
+    jears = models.ForeignKey('Jear', on_delete=models.CASCADE, verbose_name='Год')
