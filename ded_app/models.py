b0abaddf6a84922771c934b568458da2f3160a8f
stas <stas.skibickiy32@gmail.com>
2022-01-09 13:42:02 +0300

    add media files.

--- ded_app/models.py
+++ ded_app/models.py
@@ -5,7 +5,19 @@ from django.db import models
 class Jear(models.Model):
     number = models.CharField(max_length=10, verbose_name='Год')
 
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = 'Год'
+        verbose_name_plural = 'Года'
+
 
 class Foto(models.Model):
-    img = models.ImageField(verbose_name='Фото')
+    img = models.ImageField(upload_to='image/media/')
     jears = models.ForeignKey('Jear', on_delete=models.CASCADE, verbose_name='Год')
+
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
