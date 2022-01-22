671520c332b5b91f91f40269ff680da841e07f0e
stas <stas.skibickiy32@gmail.com>
2022-01-22 13:28:34 +0300

    add new fiel in view.

--- ded_app/urls.py
+++ ded_app/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import index, gallery, gallery_jear, fakt, fakt_detail
+from .views import index, gallery, gallery_jear, fakt, fakt_detail, register, login
 
 urlpatterns = [
                   path('', index, name='index'),
+                  path('register/', register, name='register'),
+                  path('login/', login, name='login'),
                   path('gallery/', gallery, name='gallery'),
                   path('gallery/<str:jears>', gallery_jear, name='gallery_jear'),
                   path('facty/', fakt, name='fakt'),
--- ded_app/views.py
+++ ded_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Foto, Jear, Fakt
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import  messages
 
 menu = [{'boss': 'Главная', 'url_name': 'index'},
         {'boss': 'Письмо Дедушке Морозу', 'url_name': 'letter'},
@@ -13,30 +15,49 @@ menu = [{'boss': 'Главная', 'url_name': 'index'},
 
 
 def index(request):
-    context = {'menu': menu}
+    context = {'menu': menu, 'boss': 'Главная'}
     return render(request, 'ded_app/base.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрированы')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регстрации - Метелица закружила-завела!')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'ded_app/register.html', context)
+
+
+def login(request):
+    return render(request, 'ded_app/login.html')
 
 
 def gallery(request):
     foto = Jear.objects.all()
-    context = {'menu': menu, 'foto': foto}
+    context = {'menu': menu, 'foto': foto, 'boss': 'Галерея'}
     return render(request, 'ded_app/gallery.html', context)
 
 
 def gallery_jear(request, jears):
     foto = Foto.objects.filter(jears=jears)
     z = Foto.objects.filter(id=jears)
-    context = {'menu': menu, 'foto': foto, 'z': z}
+    context = {'menu': menu, 'foto': foto, 'z': z, 'boss': 'Галерея'}
     return render(request, 'ded_app/gallery_jear.html', context)
 
 
 def fakt(request):
     fakt = Fakt.objects.all()
-    context = {'menu': menu, 'fakt': fakt}
+    context = {'menu': menu, 'fakt': fakt, 'boss': 'Интересные факты'}
     return render(request, 'ded_app/fakt.html', context)
 
 
 def fakt_detail(request, title):
     fakt_detail = Fakt.objects.filter(title=title)
-    context = {'menu': menu, 'fakt_detail': fakt_detail}
+    context = {'menu': menu, 'fakt_detail': fakt_detail, 'boss': title}
     return render(request, 'ded_app/fakt_detail.html', context)
